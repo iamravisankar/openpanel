b20c495437a08ee5a67395caf9c55eeff52565ad
ravi sankar <ravi.sankar@aquera.com>
2025-07-24 17:17:56 +0530

    minor fixes added

--- apps/dashboard/src/hooks/useEventNames.ts
+++ apps/dashboard/src/hooks/useEventNames.ts
@@ -1,9 +1,10 @@
 import { api } from '@/trpc/client';
 
-export function useEventNames(projectId: string) {
-  const query = api.chart.events.useQuery({
-    projectId,
+export function useEventNames(
+  params: Parameters<typeof api.chart.events.useQuery>[0],
+) {
+  const query = api.chart.events.useQuery(params, {
+    staleTime: 1000 * 60 * 10,
   });
-
   return query.data ?? [];
 }
